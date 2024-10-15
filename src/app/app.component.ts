e92f662a3ed0cb7d21165dfdcc5f91b5d1af0705
SumoPod <visfulgencio@gmail.com>
2024-10-15 22:41:12 +0200

    Coyote detects, pairs, read services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+/// <reference types="web-bluetooth" />
+
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +10,58 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BTest';
+
+  ngOnInit() {
+    // Initialization logic if any
+  }
+
+  // Function to print available, unpaired BTLE devices
+  async printUnpairedBTLEDevices() {
+    try {
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [
+          { namePrefix: 'D-LAB' }
+        ],
+        optionalServices: [
+          '955a180a-0fe2-f5aa-a094-84b8d4f3e8ad', // Battery level.
+          '955a180b-0fe2-f5aa-a094-84b8d4f3e8ad'] // Pulse writers.
+        // acceptAllDevices: true,
+      });
+      console.log('Selected device:', device.name);
+      console.log('Selected Device ID:', device.id);
+
+      if (!device.gatt)
+        {
+        throw new Error('GATT server not available on the device');
+      }
+
+      const gattServer = await device.gatt.connect();
+      console.log('Connected to GATT server:', gattServer.connected);
+      const services = await gattServer.getPrimaryServices();
+      console.log(' Post Services:');
+      for (const service of services)
+      {
+        console.log(`Service: ${service.uuid}`);
+        const characteristics = await service.getCharacteristics();
+
+        for (const characteristic of characteristics)
+        {
+          console.log(`Characteristic: ${characteristic.uuid}`);
+          const value = await characteristic.readValue();
+          console.log(`Value: ${this.bufferToHex(value.buffer)}`);
+        }
+      }
+    }
+    catch (error)
+    {
+      console.error('Error scanning for BTLE devices:', error);
+    }
+  }
+
+  // Helper function to convert buffer to hex string
+  bufferToHex(buffer: ArrayBuffer): string {
+    return Array.prototype.map.call(new Uint8Array(buffer), (x: number) => ('00' + x.toString(16)).slice(-2)).join('');
+  }
 }
